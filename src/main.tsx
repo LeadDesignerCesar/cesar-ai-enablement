c3ebaee60dc17cf61ce0f288997dc48a4b047d44
LeadDesignerCesar <mrramos206@gmail.com>
2026-08-26 23:20:56 -0400

    Load responsive production infographic styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles/global.css'
 import './styles/mockup.css'
 import './styles/refine.css'
 import './styles/event.css'
+import './styles/production.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
