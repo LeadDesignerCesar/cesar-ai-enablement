6cf8516cfa14ffc46c5f351388cd517430d49430
LeadDesignerCesar <mrramos206@gmail.com>
2026-08-26 23:51:17 -0400

    Load progressive case study styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './styles/production.css'
 import './styles/hiring.css'
 import './styles/work.css'
 import './styles/typography.css'
+import './styles/case-progressive.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
