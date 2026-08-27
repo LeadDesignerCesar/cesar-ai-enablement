96e8c00ca3e6704500ff16b5f26d7c48bfd57447
LeadDesignerCesar <mrramos206@gmail.com>
2026-08-26 23:35:43 -0400

    Load selected work layout refinements

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/refine.css'
 import './styles/event.css'
 import './styles/production.css'
 import './styles/hiring.css'
+import './styles/work.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
